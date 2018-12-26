2b68329c07081a6b9a0de7169e820a854330e41d
dovahkiin0424 <dovahkiin0424@gmail.com>
2018-12-26 16:47:12 +0100

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -7,15 +7,15 @@ from ckeditor.fields import RichTextField
 from ckeditor_uploader.fields import RichTextUploadingField
 
 class Post(models.Model):
-    cím = models.CharField(max_length=100)
+    cim = models.CharField(max_length=100)
     tartalom = RichTextUploadingField()
     kelt = models.DateTimeField(default=timezone.now)
-    szerző = models.ForeignKey(User, on_delete=models.CASCADE)
+    szerzo = models.ForeignKey(User, on_delete=models.CASCADE)
     intro = models.CharField(max_length=100, default='intro')
-    kép = models.ImageField(default='pdefault.png', upload_to='post_pics')
+    kep = models.ImageField(default='pdefault.png', upload_to='post_pics')
 
     def __str__(self):
-        return self.cím
+        return self.cim
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
