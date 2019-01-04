bd48a1b3caf1e47c936520533ede0777a3566283
dovahkiin0424 <dovahkiin0424@gmail.com>
2019-01-04 01:15:20 +0100

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 from PIL import Image
+from froala_editor.fields import FroalaField
 
 class Post(models.Model):
     cim = models.CharField(max_length=100)
-    tartalom = models.TextField()
+    tartalom = FroalaField()
     kelt = models.DateTimeField(default=timezone.now)
     szerzo = models.ForeignKey(User, on_delete=models.CASCADE)
     intro = models.CharField(max_length=100, default='intro')
