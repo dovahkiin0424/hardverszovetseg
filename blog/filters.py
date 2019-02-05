e381074393fb8ca14df74f09517f8ba70fe09062
dovahkiin0424 <dovahkiin0424@gmail.com>
2019-02-05 20:37:20 +0100

    Add files via upload

--- blog/filters.py
+++ blog/filters.py
@@ -3,19 +3,8 @@ import django_filters
 
 class PostFilter(django_filters.FilterSet):
 
-    CHOICES = (
-        ('ascending', 'Legrégebbi elől'),
-        ('descending', 'Legújabb elől')
-    )
-
-    ordering = django_filters.ChoiceFilter(label='Rendezés', choices=CHOICES, method="filter_by_order")
-
     class Meta:
         model = Post
         fields = {
             'cim': ['icontains'],
         }
-
-    def filter_by_order(self, queryset, name, value):
-        expression = 'kelt' if value == 'ascending' else '-kelt'
-        return queryset.order_by(expression)
